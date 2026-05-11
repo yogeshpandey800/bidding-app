3dab0b3cacc8c45d7a70cd97535f574ca014468b
Yogesh <ypandey041@gmail.com>
2026-05-11 09:41:00 +0530

    fixed connection.js

--- server/connection.js
+++ server/connection.js
@@ -12,18 +12,3 @@ export const connectDB = async () => {
 }
 
 
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// export const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URL, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         });
-//         console.log('✅ Connected to MongoDB');
-//     } catch (error) {
-//         console.error('❌ Error connecting to MongoDB:', error.message);
-//     }
-// };
